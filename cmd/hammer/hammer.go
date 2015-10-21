f4d8e6f04e721db7e1873805ab1867ac1beefdc2
jayvyas <jay@apache.org>
2015-10-21 05:48:24 -0400

    a port of the gc controller just for playing with hammering the api server

--- cmd/hammer/hammer.go
+++ cmd/hammer/hammer.go
@@ -17,18 +17,153 @@ limitations under the License.
 package main
 
 import (
-	//"os"
 	"fmt"
-	//"runtime"
-	//"k8s.io/kubernetes/pkg/kubectl/cmd"
-	//cmdutil "k8s.io/kubernetes/pkg/kubectl/cmd/util"
+	"k8s.io/kubernetes/pkg/api"
+	//"k8s.io/kubernetes/pkg/api/unversioned"
+	"k8s.io/kubernetes/pkg/client/cache"
+	"k8s.io/kubernetes/pkg/client/record"
+	client "k8s.io/kubernetes/pkg/client/unversioned"
+	"k8s.io/kubernetes/pkg/client/unversioned/clientcmd"
+	clientcmdapi "k8s.io/kubernetes/pkg/client/unversioned/clientcmd/api"
+	//"k8s.io/kubernetes/pkg/controller"
+	"k8s.io/kubernetes/pkg/controller/framework"
+	"k8s.io/kubernetes/pkg/fields"
+	"k8s.io/kubernetes/pkg/labels"
+	"k8s.io/kubernetes/pkg/runtime"
+	"k8s.io/kubernetes/pkg/util"
+	"k8s.io/kubernetes/pkg/watch"
+	"os"
+	"sort"
+	"sync"
+	"time"
+
+	"github.com/golang/glog"
 )
 
+const (
+	gcCheckPeriod = 20 * time.Second
+	gcThreshold   = 10
+)
+
+type GCController struct {
+	kubeClient     client.Interface
+	podStore       cache.StoreToPodLister
+	podStoreSyncer *framework.Controller
+	deletePod      func(namespace, name string) error
+	threshold      int
+}
+
+func New(kubeClient client.Interface) *GCController {
+
+	resyncPeriodFunc := func() time.Duration {
+		return 1 * time.Second
+	}
+
+	eventBroadcaster := record.NewBroadcaster()
+	eventBroadcaster.StartLogging(glog.Infof)
+	eventBroadcaster.StartRecordingToSink(kubeClient.Events(""))
+
+	gcc := &GCController{
+		kubeClient: kubeClient,
+		threshold:  gcThreshold,
+		deletePod: func(namespace, name string) error {
+			return kubeClient.Pods(namespace).Delete(name, api.NewDeleteOptions(0))
+		},
+	}
+
+	terminatedSelector := compileTerminatedPodSelector()
+
+	gcc.podStore.Store, gcc.podStoreSyncer = framework.NewInformer(
+		&cache.ListWatch{
+			ListFunc: func() (runtime.Object, error) {
+				return gcc.kubeClient.Pods(api.NamespaceAll).List(labels.Everything(), terminatedSelector)
+			},
+			WatchFunc: func(rv string) (watch.Interface, error) {
+				return gcc.kubeClient.Pods(api.NamespaceAll).Watch(labels.Everything(), terminatedSelector, rv)
+			},
+		},
+		&api.Pod{},
+		resyncPeriodFunc(),
+		framework.ResourceEventHandlerFuncs{},
+	)
+	return gcc
+}
+
+func (gcc *GCController) Run(stop <-chan struct{}) {
+	go gcc.podStoreSyncer.Run(stop)
+	go util.Until(gcc.gc, gcCheckPeriod, stop)
+	<-stop
+}
+
+func (gcc *GCController) gc() {
+	terminatedPods, _ := gcc.podStore.List(labels.Everything())
+	terminatedPodCount := len(terminatedPods)
+	sort.Sort(byCreationTimestamp(terminatedPods))
+
+	deleteCount := terminatedPodCount - gcc.threshold
+
+	if deleteCount > terminatedPodCount {
+		deleteCount = terminatedPodCount
+	}
+	if deleteCount > 0 {
+		glog.Infof("garbage collecting %v pods", deleteCount)
+	}
+
+	var wait sync.WaitGroup
+	for i := 0; i < deleteCount; i++ {
+		wait.Add(1)
+		go func(namespace string, name string) {
+			defer wait.Done()
+			if err := gcc.deletePod(namespace, name); err != nil {
+				// ignore not founds
+				defer util.HandleError(err)
+			}
+		}(terminatedPods[i].Namespace, terminatedPods[i].Name)
+	}
+	wait.Wait()
+}
+
+func compileTerminatedPodSelector() fields.Selector {
+	selector, err := fields.ParseSelector("status.phase!=" + string(api.PodPending) + ",status.phase!=" + string(api.PodRunning) + ",status.phase!=" + string(api.PodUnknown))
+	if err != nil {
+		panic("terminatedSelector must compile: " + err.Error())
+	}
+	return selector
+}
+
+// byCreationTimestamp sorts a list by creation timestamp, using their names as a tie breaker.
+type byCreationTimestamp []*api.Pod
+
+func (o byCreationTimestamp) Len() int      { return len(o) }
+func (o byCreationTimestamp) Swap(i, j int) { o[i], o[j] = o[j], o[i] }
+func (o byCreationTimestamp) Less(i, j int) bool {
+	if o[i].CreationTimestamp.Equal(o[j].CreationTimestamp) {
+		return o[i].Name < o[j].Name
+	}
+	return o[i].CreationTimestamp.Before(o[j].CreationTimestamp)
+}
+
 func main() {
-	//runtime.GOMAXPROCS(runtime.NumCPU())
-	//cmd := cmd.NewKubectlCommand(cmdutil.NewFactory(nil), os.Stdin, os.Stdout, os.Stderr)
-	//if err := cmd.Execute(); err != nil {
-	//	os.Exit(1)
-	//}
-	fmt.Print("ASDFA")
+	// This creates a client, first loading any specified kubeconfig
+	// file, and then overriding the Master flag, if non-empty.
+	kubeconfig, err := clientcmd.NewNonInteractiveDeferredLoadingClientConfig(
+		&clientcmd.ClientConfigLoadingRules{ExplicitPath: "/opt/kube-config"},
+		&clientcmd.ConfigOverrides{ClusterInfo: clientcmdapi.Cluster{Server: "127.0.0.1"}}).ClientConfig()
+	if err != nil {
+		fmt.Print("FAILED!!!!!!!")
+		os.Exit(1)
+	}
+
+	// Override kubeconfig qps/burst settings from flags
+	kubeconfig.QPS = 100
+	kubeconfig.Burst = 100
+
+	kubeClient, err := client.New(kubeconfig)
+	ctrlr := New(kubeClient)
+
+	ch := make(chan struct{})
+	fmt.Print("Starting controller. ")
+	ctrlr.Run(ch)
+	fmt.Prunt("... DONE.")
+
 }
