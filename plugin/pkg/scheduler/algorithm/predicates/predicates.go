54946377bdefb707c984f4a52180a6eaa8143dc9
jayunit100 <jay@apache.org>
2016-10-07 14:53:18 -0400

    ASDF

--- plugin/pkg/scheduler/algorithm/predicates/predicates.go
+++ plugin/pkg/scheduler/algorithm/predicates/predicates.go
@@ -747,7 +747,9 @@ func (s *ServiceAffinity) CheckServiceAffinity(pod *api.Pod, meta interface{}, n
 
 		// Optimization
 		if predicateMeta.podServices[pod.Namespace+labels.Set(pod.Labels).String()] == nil {
-				predicateMeta.podServices[pod.Namespace+labels.Set(pod.Labels).String()], err = s.serviceLister.GetPodServices(pod)
+			predicateMeta.lock.Lock()
+			defer predicateMeta.lock.Unlock()
+			predicateMeta.podServices[pod.Namespace+labels.Set(pod.Labels).String()], err = s.serviceLister.GetPodServices(pod)
 		}
 		services := predicateMeta.podServices[pod.Namespace+labels.Set(pod.Labels).String()]
 		// fmt.Println("CHECKED SERVICES :::::::::::: pod ", pod, "pod spec", pod.Spec, "services matching = " , services, "error=", err)
