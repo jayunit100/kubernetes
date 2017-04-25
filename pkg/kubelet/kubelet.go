5c54347689c7394c1d7b0ddbef50efd74be0aacc
jayunit100 <jay@apache.org>
2017-04-25 18:15:41 +0000

    Kubelet: Reactor refactoring and exponential backoff
    backoff stuff
    
    backoff stuff

--- pkg/kubelet/kubelet.go
+++ pkg/kubelet/kubelet.go
@@ -110,8 +110,8 @@ const (
 	// Max amount of time to wait for the container runtime to come up.
 	maxWaitForContainerRuntime = 30 * time.Second
 
-	// nodeStatusUpdateRetry specifies how many times kubelet retries when posting node status failed.
-	nodeStatusUpdateRetry = 5
+	// nodeStatusUpdateRetryBackoff is the amount of time that we are willing to spend retrying to connect to the apiserver.
+	nodeStatusUpdateRetryMaximumTime = 20 * time.Minute
 
 	// Location of container logs.
 	ContainerLogsDir = "/var/log/containers"
@@ -822,6 +822,7 @@ func NewMainKubelet(kubeCfg *componentconfig.KubeletConfiguration, kubeDeps *Kub
 	// Finally, put the most recent version of the config on the Kubelet, so
 	// people can see how it was configured.
 	klet.kubeletConfiguration = *kubeCfg
+	go klet.backoffNotifier()
 	return klet, nil
 }
 
@@ -1124,7 +1125,8 @@ type Kubelet struct {
 
 	// dockerLegacyService contains some legacy methods for backward compatibility.
 	// It should be set only when docker is using non json-file logging driver.
-	dockerLegacyService dockershim.DockerLegacyService
+	dockerLegacyService    dockershim.DockerLegacyService
+	backoffNotifierRunning bool
 }
 
 // setupDataDirs creates:
