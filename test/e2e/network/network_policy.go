fcdab1678827af45a1f7f336787843af5b150e73
jay vyas <jay@apache.org>
2020-05-26 09:05:02 -0400

    compiling WIP

--- test/e2e/network/network_policy.go
+++ test/e2e/network/network_policy.go
@@ -328,7 +328,7 @@ var _ = SIGDescribe("NetworkPolicy [LinuxOnly]", func() {
 			reachability := netpol.NewReachability(scenario.allPods, true)
 			// disallow all traffic from the x or z namespaces.. but allow 'pod:b' and 'ns:y'
 			scenario.forEach(func(from, to netpol.PodString){
-				if to.Namespace() == "x" {
+				if to.Namespace() == "x" && to.PodName()== "a"{
 					reachability.Expect(from, to, from.Namespace()=="z" || from.PodName()=="b")
 				}
 			})
