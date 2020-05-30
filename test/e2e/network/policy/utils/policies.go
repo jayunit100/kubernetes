479efe852682ce579833217a8053e76623c18724
Akash Sarda <akashsarda3@gmail.com>
2020-05-30 18:29:00 +0530

    xyz

--- test/e2e/network/policy/utils/policies.go
+++ test/e2e/network/policy/utils/policies.go
@@ -150,3 +150,34 @@ func GetDefaultAllAllowEggress(name string) *networkingv1.NetworkPolicy {
 		},
 	}
 }
+
+func GetPolicyWithEgressRuleOnlyPodSelector(ns string, name string, toPod string ) *networkingv1.NetworkPolicy{
+	return &networkingv1.NetworkPolicy{
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: ns,
+			Name:     name,
+		},
+		Spec: networkingv1.NetworkPolicySpec{
+			// Apply this policy to the client
+			PodSelector: metav1.LabelSelector{
+				MatchLabels: map[string]string{
+					"pod": name,
+				},
+			},
+			PolicyTypes: []networkingv1.PolicyType{networkingv1.PolicyTypeEgress},
+			Egress: []networkingv1.NetworkPolicyEgressRule{
+				{
+					To: []networkingv1.NetworkPolicyPeer{
+						{
+							PodSelector: &metav1.LabelSelector{
+								MatchLabels: map[string]string{
+										"pod": toPod
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+}
